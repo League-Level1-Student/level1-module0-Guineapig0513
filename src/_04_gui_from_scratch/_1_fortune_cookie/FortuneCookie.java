773249d93f71ec5612788acb3b5beb79d2bcc68e
Aarit Jayaswal <aaritrj@gmail.com>
2026-01-10 16:23:25 -0800

    6tr6o

--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -1,11 +1,12 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
-public class fortuneCookie {
+import javax.swing.JFrame;
 
+public class FortuneCookie {
+	JFrame frame = new JFrame();
 	public void showButton() {
 		System.out.println("Button clicked");
-		
-
+		frame.setVisible(true);
 	}
 
 }
--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookieRunner.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookieRunner.java
@@ -1,7 +1,7 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
-public class fortuneCookieRunner {
+public class FortuneCookieRunner {
     public static void main(String[] args) {
-        new fortuneCookie().showButton();
+        new FortuneCookie().showButton();
   }
 }
