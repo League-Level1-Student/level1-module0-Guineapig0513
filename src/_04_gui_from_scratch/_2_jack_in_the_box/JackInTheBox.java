821326467ee057e48c7633ebe68c1f8c58967890
Aarit Jayaswal <aaritrj@gmail.com>
2026-01-31 16:24:16 -0800

    gnsshtsth

--- src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
@@ -1,14 +1,22 @@
 package _04_gui_from_scratch._2_jack_in_the_box;
 
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class JackInTheBox implements ActionListener {
 	JButton button = new JButton("SUPRISE!!!");
 	JFrame frame = new JFrame("supa mysterious button :O");
+	int i = 0;		
 	public static void main(String[] args) {
 		new JackInTheBox().run();
 
@@ -23,11 +31,53 @@ public class JackInTheBox implements ActionListener {
 		          frame.setVisible(true);
 		          button.addActionListener(this);
 	}
-
+	private void showPicture(String fileName) {
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-				
+		i += 1;
+		if(i == 5) {
+		System.out.println("you clicked the button "+i+" times");
+		showPicture("jackInTheBox.png");
+		playSound("homer-woohoo.wav");
+		JOptionPane.showMessageDialog(null, "haha");
+		System.exit(0);
 	}
-}
+	}
 
+private void playSound(String soundFile) {
+    try {
+         AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+         sound.play();
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+}
