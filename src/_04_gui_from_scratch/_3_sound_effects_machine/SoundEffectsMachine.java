fca58c46bec5f44a19dc2207a9b9e71579a7d85b
Aarit Jayaswal <aaritrj@gmail.com>
2026-02-21 16:22:21 -0800

    dhhddh

--- src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/SoundEffectsMachine.java
@@ -2,9 +2,13 @@ package _04_gui_from_scratch._3_sound_effects_machine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SoundEffectsMachine implements ActionListener {
@@ -16,6 +20,10 @@ public class SoundEffectsMachine implements ActionListener {
 	JFrame frame = new JFrame("WOAHHHH A SOUND MACHINE???");
 	JPanel panel = new JPanel();
 	
+	JButton yes = new JButton("ACCEPT");
+	JButton no = new JButton("DECLINE");
+	JFrame fr4me = new JFrame ("NEW NUMBER");
+	JPanel p4nel = new JPanel();
 	public void run() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
@@ -32,8 +40,53 @@ public class SoundEffectsMachine implements ActionListener {
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+	
+		if (button1 == e.getSource()) {
+			fr4me.add(p4nel);
+			fr4me.pack();
+			p4nel.add(yes);
+			p4nel.add(no);
+			fr4me.pack();
+			fr4me.setVisible(true);
+			yes.addActionListener(this);
+			no.addActionListener(this);
+			JOptionPane.showMessageDialog(null, "NEW CONTACT CALLED 'Spamton G Spamton' SENT A VOICEMAIL, ACCEPT?");
+		}
+		if(yes == e.getSource()) {
+			playSound("spam.wav");
+			JOptionPane.showMessageDialog(null, "oh noes! you got scammed :'[.. somehow?");;
+		}
+		else if(no == e.getSource()) {
+			fr4me.setVisible(false);
+		}
+		else if (button2 == e.getSource()) {
+			JOptionPane.showMessageDialog(null, "Oh look the news is on, on that suspiously purple TV! it kinda sounds like TV TIME...");
+			playSound("news.wav");
+		}
+		else {
+			playSound("wood.wav");
+		}
+	}
+	private void playSound(String soundFile) {
+		String path = "src/_04_gui_from_scratch/_3_sound_effects_machine/";
+			File sound = new File(path+soundFile);
+			if (sound.exists()) {
+				new Thread(() -> {
+				try {
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(sound));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+				}
+				catch (Exception e) {
+					System.out.println("Could not play this sound");
+				}}).start();
+	 		}
+			else {
+				System.out.println("File does not exist");
+			}
 		
 	}
 
---
+++ src/_04_gui_from_scratch/_4_cuteness_tv/CutenessTV.java
@@ -0,0 +1,10 @@
+package _04_gui_from_scratch._4_cuteness_tv;
+
+public class CutenessTV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
